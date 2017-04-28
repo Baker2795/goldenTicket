6256cd5ef971a84efafbb062192f4af90545da63
John Baker <john@b4k3r.com>
2017-04-28 13:27:17 -0400

    Initial Commit

---
+++ goldenTicket/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  goldenTicket
+//
+//  Created by John Baker on 4/28/17.
+//  Copyright © 2017 B4k3R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
